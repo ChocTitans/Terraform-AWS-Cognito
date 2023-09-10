17c27e3e3b170cbd6bd4c746e75a29f26e48712a
Hamza <choctitanz@gmail.com>
2023-09-10 16:57:28 +0100

    testing again

--- config.tf
+++ config.tf
@@ -5,11 +5,6 @@ terraform {
         key = "backend.tfstate"
         encrypt = true
         dynamodb_table = "tf-backend"
-
-        region     = jsondecode(file("$secrets.auto.tfvars")).region
-        access_key = jsondecode(file("$secrets.auto.tfvars")).access_key
-        secret_key = jsondecode(file("$secrets.auto.tfvars")).secret_key
-        
     }
 
 }
