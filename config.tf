c0f134f4a26932727f16dcafd83e9ff9130e1d01
Hamza <choctitanz@gmail.com>
2023-09-12 15:43:51 +0100

    test secrets

--- config.tf
+++ config.tf
@@ -3,11 +3,11 @@ terraform {
     backend "s3" {
         bucket = "cognito-terraform-backend"
         key = "backend.tfstate"
-        region = "eu-west-2"
+        region = "${var.aws_region}"
         encrypt = true
         dynamodb_table = "tf-backend"
-        access_key = "AKIATLJMO3UH4XGZRW67"
-        secret_key = "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"
+        access_key = "${var.aws_access_key}"
+        secret_key = "${var.aws_secret_key}"
     }
 
 }
