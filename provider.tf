bd3d43834f2e708524b28b15b86634c6fb5fa7cb
Hamza <choctitanz@gmail.com>
2023-09-12 16:10:40 +0100

    test secrets

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-    region = ${var.aws_region}
-    access_key = ${var.aws_access_key_id}
-    secret_key = ${var.aws_secret_key_id}
+    region = var.aws_region
+    access_key = var.aws_access_key_id
+    secret_key = var.aws_secret_key_id
 }
