df9264a1ee0633a4778c61c5e849a678702e05ad
Hamza <choctitanz@gmail.com>
2023-09-12 15:55:43 +0100

    test secrets

--- config.tf
+++ config.tf
@@ -6,8 +6,6 @@ terraform {
         region = "${var.aws_region}"
         encrypt = true
         dynamodb_table = "tf-backend"
-        access_key = "${var.aws_access_key}"
-        secret_key = "${var.aws_secret_key}"
     }
 
 }
