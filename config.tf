485027d97e414730ff3b6e31b25eaa2bd3ae8b77
Hamza <choctitanz@gmail.com>
2023-09-12 16:01:51 +0100

    test secrets

--- config.tf
+++ config.tf
@@ -3,11 +3,8 @@ terraform {
     backend "s3" {
         bucket = "cognito-terraform-backend"
         key = "backend.tfstate"
-        region = "${var.aws_region}"
         encrypt = true
         dynamodb_table = "tf-backend"
-        access_key = "${var.aws_access_key}"
-        secret_key = "${var.aws_secret_key}"
     }
 
 }
---
+++ secrets.auto.tfvars
@@ -0,0 +1,3 @@
+aws_region      = "eu-west-2"  # Replace with your desired region
+aws_access_key  = "AKIATLJMO3UH4XGZRW67"
+aws_secret_key  = "FTaNYtsqCrKIJt6C0DZC6qvFp1CgMN4Lr37ftMbu"
