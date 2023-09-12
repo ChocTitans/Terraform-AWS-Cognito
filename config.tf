9169672a890fa487bea3618f8bbd0f9443863377
Hamza <choctitanz@gmail.com>
2023-09-12 14:55:36 +0100

    added backend s3

--- config.tf
+++ config.tf
@@ -5,8 +5,8 @@ terraform {
         key = "backend.tfstate"
         region = "eu-west-2"
         encrypt = true
-        dynamodb_table = "tf-backend"
-        profile = "ttv-profile"
+        access_key = "AKIATLJMO3UH4XGZRW67"
+        secret_key = "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
     }
 
 }
