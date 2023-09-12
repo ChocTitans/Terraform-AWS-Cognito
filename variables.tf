aaad9d414c8f5d70336004ee56b99b87f60d5905
Hamza <choctitanz@gmail.com>
2023-09-12 16:27:17 +0100

    test secrets

--- variables.tf
+++ variables.tf
@@ -1,11 +1,3 @@
 variable "aws_region" {
   description = "The AWS region"
 }
-
-variable "aws_access_key_id" {
-  description = "The AWS access key"
-}
-
-variable "aws_secret_key_id" {
-  description = "The AWS secret key"
-}
